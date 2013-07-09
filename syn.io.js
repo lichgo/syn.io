f37acf3671ac83fe17b3868b0298909a398921c1
lichgo <lichgo88@gmail.com>
2013-07-09 16:54:15 +0800

    main file

---
+++ syn.io.js
@@ -0,0 +1,63 @@
+var syn = syn || (function(WS) {
+	if (WS === null) 
+		throw {
+			name: 'WebSocket Error',
+			message: 'Your browser does not support the function. Please use chrome.'
+		}
+
+	var instance,
+
+		subscribers = {
+			'open': [],
+			'message': [],
+			'close': []
+		},
+
+		connect = function(server) {
+			instance = new WS(server);
+			for (var type in subscribers) {
+				if (subscribers.hasOwnProperty(type)) {
+					instance['on' + type] = (function(type) {
+						return function(event) {
+							var i = 0, callbacks = subscribers[type], len = callbacks.length;
+							for (; i < len; i++) {
+								callbacks[i].call(instance, event);
+							}
+						}
+					})(type);
+				}
+			}
+
+			connect = function() {
+				return this;
+			};
+
+			return this;
+		},
+
+		getInstance = function() {
+			return instance;
+		},
+
+		on = function(type, callback) {
+			if (subscribers[type]) {
+				subscribers[type].push(callback);
+			}
+			return this;
+		},
+
+		send = function(data) {
+			if (instance) {
+				instance.send(data);
+			}
+			return this;
+		};
+
+	return {
+		connect: connect,
+		getInstance: getInstance,
+		on: on,
+		send: send
+	};
+
+})(('WebSocket' in window) ? WebSocket : null);
